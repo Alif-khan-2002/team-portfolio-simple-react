44d7b60f5e5627f102087191efc97c7b2fc1dabc
Alif-khan-2002 <alifr2648@gmail.com>
2024-12-29 00:48:34 +0600

    alif

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
   
   theme: {
     extend: {},
+    fontFamily: {
+      custom: ['"Inknut Antiqua"', 'ui-serif', 'Georgia', 'serif'], // Customize as needed
+    },
   },
   plugins: [],
 }
